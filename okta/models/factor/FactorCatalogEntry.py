6c3154f1c87e2ee34fcb021119c924fc1ed7d915
Mike Wallick <mike.wallick@okta.com>
2020-05-28 14:56:21 -0500

    Adds missing fields for completeness

--- okta/models/factor/FactorCatalogEntry.py
+++ okta/models/factor/FactorCatalogEntry.py
@@ -1,8 +1,21 @@
+from okta.models.Link import Link
+
+
 class FactorCatalogEntry:
 
     types = {
         'factorType': str,
-        'provider': str
+        'provider': str,
+        'status': str,
+        'enrollment': str
+    }
+
+    dict_types = {
+        '_links': Link
+    }
+
+    alt_names = {
+        '_links': 'links'
     }
 
     def __init__(self):
@@ -10,3 +23,9 @@ class FactorCatalogEntry:
         self.factorType = None  # str
 
         self.provider = None  # str
+
+        self.status = None  # str
+
+        self.enrollment = None  # str
+
+        self.links = None  # Map<String, LinksUnion>
