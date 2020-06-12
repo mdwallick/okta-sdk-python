ed81284509798ef511f7f6cec9a666a645569e21
Mike Wallick <mike.wallick@okta.com>
2020-06-12 15:08:01 -0500

    adds try/except to get_user

--- examples/user_get.py
+++ examples/user_get.py
@@ -9,8 +9,12 @@ from script_config import base_url, api_token, user_id
 usersClient = UsersClient(
     base_url=base_url, api_token=api_token)
 
-print("Get User")
-user = usersClient.get_user(user_id)
-print("ID: {0}".format(user.id))
-print("Status: {0}".format(user.status))
-print("{0}\n".format(json.dumps(user.profile, cls=Serializer, indent=2)))
+try:
+    print("Get User")
+    user = usersClient.get_user(user_id)
+    print("ID: {0}".format(user.id))
+    print("Status: {0}".format(user.status))
+    print("{0}\n".format(json.dumps(user.profile, cls=Serializer, indent=2)))
+except OktaError as e:
+    print(e.error_summary)
+    print(e.error_causes)
